63b5bc5c7bd5deb197f1001509837e99321f53ad
knvishal <vik@narola.email>
2019-09-26 10:15:50 +0530

    Employer service change

---
+++ frontend/src/app/views/employer/employer.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+@Injectable({
+    providedIn: 'root'
+})
+export class EmployerService {
+    private url = 'http://localhost:3000/employer';
+    constructor(private http: HttpClient, private route: Router) { }
+
+   // offer
+    view_offer(): Observable<any[]> {
+        return this.http.get<any[]>(`${this.url}` + '/offer/view_offer');
+    }
+
+    // candidate
+
+    view_approved_candidate(): Observable<any[]> {
+        return this.http.get<any[]>(`${this.url}` + '/manage_candidate/approved_candidate');
+    }
+
+    // Sub-Accounts
+
+    view_sub_account(): Observable<any[]> {
+        return this.http.get<any[]>(`${this.url}` + '/view_sub_accounts');
+    }
+}
