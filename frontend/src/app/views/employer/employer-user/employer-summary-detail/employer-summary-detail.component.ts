12e6381cb5436739ef1a77b0b6ecb3e77c31ac1b
knvishal <vik@narola.email>
2019-10-08 14:41:04 +0530

    Add one function in employer-summary-detail.component.ts

--- frontend/src/app/views/employer/employer-user/employer-summary-detail/employer-summary-detail.component.ts
+++ frontend/src/app/views/employer/employer-user/employer-summary-detail/employer-summary-detail.component.ts
@@ -21,41 +21,16 @@ export class EmployerSummaryDetailComponent implements OnInit {
   ngOnInit() {
      this.route.params.subscribe((params: Params) => {
       this.id = params['id'];
-      console.log(this.id);
     });
 
     this.service.offer_detail(this.id).subscribe(res => {
-
       this.offers = res['data'];
-      console.log('detail duration', this.offers);
-
-      // location
-      // this.service.get_location().subscribe(res => {
-      //   this.location = res['data']['data'];
-      //   this.loc = this.location.filter(x => x._id === this.offers.location);
-      // });
-
-      // group
-
-      // this.service.get_groups().subscribe(res => {
-      //     this.group = res['data']['data'];
-
-      //     this.group = this.group.filter(x => x._id === this.offers.group);
-      //     console.log(this.group);
-      // });
-
-      // salary
-
-      // this.service.get_salary_brcaket().subscribe(res => {
-      //   this.salary = res['data']['data'];
-      //   this.displayForm = true;
-      //   this.salary = this.salary.filter(x => x._id === this.offers.salarybracket);
-      //   console.log(this.salary);
-      // });
-
     });
   }
 
+  checkValue(e) {
+    console.log(e);
+  }
 
   edit(id) {
     this.router.navigate(['/employer/manage_offer/addoffer/' + id]);
