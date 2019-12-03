e47cfd1493433eeb9c9061fa5107468ce925e523
Mansi <37538434+modi-mansi@users.noreply.github.com>
2019-12-03 16:50:27 +0530

    changes  for email

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -545,8 +545,11 @@ router.post("/employer_register", async (req, res) => {
 });
 
 router.post("/check_employer_email", async (req, res) => {
-
-  let user_resp = await common_helper.findOne(User, { "email": req.body.email.toLowerCase(), "is_del": false })
+  re = new RegExp(req.body.email, "i");
+  value = {
+    $regex: re
+  };
+  let user_resp = await common_helper.findOne(User, { "email": value, "is_del": false })
   if (user_resp.status === 1) {
     res.status(config.BAD_REQUEST).json({ "status": 0, "message": "Email address already Register" });
   } else {
@@ -557,7 +560,11 @@ router.post("/check_employer_email", async (req, res) => {
 
 router.post("/email_exists", async (req, res) => {
   var user_id = req.body.user_id;
-  var user_resp = await common_helper.findOne(User, { "_id": { $ne: ObjectId(user_id) }, "email": req.body.email.toLowerCase(), "is_del": false })
+  re = new RegExp(req.body.email, "i");
+  value = {
+    $regex: re
+  };
+  var user_resp = await common_helper.findOne(User, { "_id": { $ne: ObjectId(user_id) }, "email": value, "is_del": false })
   if (user_resp.status === 1) {
     res.status(config.BAD_REQUEST).json({ "status": 0, "message": "Email address already Register" });
   }
