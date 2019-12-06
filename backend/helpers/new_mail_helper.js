02895e4b3ae6bf2cd553718cd98bf924b8801e9d
knvishal <vik@narola.email>
2019-12-06 18:07:26 +0530

    modify cron

--- backend/helpers/new_mail_helper.js
+++ backend/helpers/new_mail_helper.js
@@ -3,7 +3,6 @@ var EmailTemplate = require('email-templates').EmailTemplate;
 const bcrypt = require('bcryptjs');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 const sgMail = require('@sendgrid/mail');
-var mail_api_key = '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';
 var request = require("request");
 var new_mail_helper = {};
 var config = require("./../config");
