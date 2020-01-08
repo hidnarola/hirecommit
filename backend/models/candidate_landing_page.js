4a7c3e0314623adcbcb0a63df0ad2ea193b6ef8f
knvishal <vik@narola.email>
2020-01-08 10:16:49 +0530

    changes

--- backend/models/candidate_landing_page.js
+++ backend/models/candidate_landing_page.js
@@ -7,27 +7,33 @@ const CandidateLandingPageSchema = new Schema({
         main_heading_1: { type: String },
         main_heading_2: { type: String },
         content_1: { type: String },
-        content_2: { type: String }
+        content_2: { type: String },
+        image: { type: String }
     },
     signup_for_free: {
         heading_1: { type: String },
-        content_1: { type: String }
+        content_1: { type: String },
+        image: { type: String }
     },
     receive_offers: {
         heading_1: { type: String },
-        content_1: { type: String }
+        content_1: { type: String },
+        image: { type: String }
     },
     accept_offer: {
         heading_1: { type: String },
-        content_1: { type: String }
+        content_1: { type: String },
+        image: { type: String }
     },
     join_company: {
         heading_1: { type: String },
-        content_1: { type: String }
+        content_1: { type: String },
+        image: { type: String }
     },
     data_security: {
         heading_1: { type: String },
-        content_1: { type: String }
+        content_1: { type: String },
+        image: { type: String }
     },
     transparency: {
         heading_1: { type: String },
@@ -38,7 +44,8 @@ const CandidateLandingPageSchema = new Schema({
     support_identity: {
         heading_1: { type: String },
         heading_2: { type: String },
-        content_1: { type: String }
+        content_1: { type: String },
+        image: { type: String }
     },
     is_del: {
         type: Boolean,
